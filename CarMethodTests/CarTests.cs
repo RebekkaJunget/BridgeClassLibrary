0e2afb4016c26b5ffc137e5a53b0a7e3ad9b7695
Rebekka Rendow Junget <rej003@edu.zealand.dk>
2025-03-05 11:38:51 +0100

    Unite test på Price() og VihecleType() metoder

---
+++ CarMethodTests/CarTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BridgeClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeClassLibrary.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        [TestMethod()]
+
+        /// <summary>
+        /// Unit test for the Price method in the Car class
+        /// <summary>
+        public void PriceMethodTest()
+        {
+            
+            //Arrange
+            Car car = new Car();
+            //Act
+            double actual = car.Price();
+
+            //Assert
+            Assert.AreEqual(230, actual, "Burde retunere 230.-");
+
+        }
+
+        [TestMethod()]
+        ///<summary>    
+        ///Unit test for the VehicleType method in the Car class
+        ///<!--<summary>-->
+        
+        public void VihecleTypeMethodTest()
+        {
+            //Arrange
+            Car car = new Car();
+            //Act
+            string actual = car.VehicleType();
+            //Assert
+            Assert.AreEqual("Car", actual, "Burde returnere Car");
+
+        }
+    }
+}
