48d9bce14bf68391b94badaed197d53ec7fe24cf
Rebekka Rendow Junget <rej003@edu.zealand.dk>
2025-03-05 11:46:24 +0100

    Created class "MC" og unit testet begge metoder - Price() + VehicleType()

---
+++ BridgeClassLibrary/MC.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeClassLibrary
+{
+    /// <summary>
+    /// Class MC with properties Licenseplate and date
+    /// </summary>
+    public class MC
+    {
+        public string Licenseplate { get; set; }
+
+        public DateTime date { get; set; }
+
+        /// <summary>
+        /// constructor that takes two parameters licenseplate and date
+        /// </summary>
+        /// <param name="licenseplate"></param>
+        /// <param name="date"></param>
+        public MC(string licenseplate, DateTime date)
+        {
+            Licenseplate = licenseplate;
+            this.date = date;
+        }
+
+        /// <summary>
+        /// default constructor
+        /// </summary>
+        public MC()
+        {
+        }
+
+        /// <summary>
+        /// method that returns the price of for crossing the bridge
+        /// </summary>
+        /// <returns></returns>
+        public double Price()
+        {
+            return 120;
+        }
+        /// <summary>
+        /// method that returns the type of vehicle
+        /// </summary>
+        /// <returns></returns>
+        public string VehicleType()
+        {
+            return "MC";
+        }
+    }
+}
---
+++ MCMethodTests/MCTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BridgeClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeClassLibrary.Tests
+{
+    [TestClass()]
+    public class MCTests
+    {
+        /// <summary>
+        /// unit test for the Price method in the MC class
+        /// </summary>
+        [TestMethod()]
+        public void PriceMethodTest()
+        {
+            //Arrange
+            MC mc = new MC();
+            //Act
+            double actual = mc.Price();
+            //Assert
+            Assert.AreEqual(120, actual, "Burde retunere 120.-");
+        }
+
+        /// <summary>
+        /// unit test for the VehicleType method in the MC class
+        /// </summary>'
+        [TestMethod()]
+        public void VehicleTypeMethodTest()
+        {
+            //Arrange
+            MC mc = new MC();
+            //Act
+            string actual = mc.VehicleType();
+            //Assert
+            Assert.AreEqual("MC", actual, "Burde returnere MC");
+        }
+    }
+}
