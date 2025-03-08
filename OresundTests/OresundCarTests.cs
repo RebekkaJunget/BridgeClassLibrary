3e00bf7be328ac6695092c0110b002ccb0e5ddc1
Rebekka Rendow Junget <rej003@edu.zealand.dk>
2025-03-08 21:03:06 +0100

    Unit test on Oresundbroen - OresundCarTest (price and vehicle works)

---
+++ OresundTests/OresundCarTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Oresundbroen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oresundbroen.Tests
+{
+    [TestClass()]
+    public class OresundCarTests
+    {
+        [TestMethod()]
+        public void OresundCar_WithoutBroBizz_ShouldReturnFullPriceTest()
+        {
+            // Arrange
+            OresundCar car = new OresundCar("AD12745", new DateTime(2020, 10, 5)); // Weekday, No BroBizz
+            // Act
+            double actual = car.Price();
+            // Assert
+            Assert.AreEqual(460, actual, 0.01);
+        }
+
+        [TestMethod()]
+
+        public void OresundCar_VehicleType_ShouldReturnCorrectString()
+        {
+            //Arrange
+            OresundCar car = new OresundCar("123ABC", new DateTime(2020, 10, 5));
+
+            //Act
+            string actual = car.VehicleType();
+
+            //Assert
+            Assert.AreEqual("Oresund Car", actual);
+
+        }
+
+        
+    }
+}
