2ea3359e0229d3eb84d630d7b4e039e8a2c8775d
Rebekka Rendow Junget <rej003@edu.zealand.dk>
2025-03-08 20:01:58 +0100

    Added new Library "StoreBaeltTicketLibrary". Ref to externDLL Added override methode for weekend dicount in class "StoreBaeltCar"

---
+++ StoreBaeltTicketLibrary/StoreBaeltCar.cs
@@ -0,0 +1,41 @@
+﻿using BridgeClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreBaeltTicketLibrary
+{
+    public class StoreBaeltCar : Car
+    {
+        public StoreBaeltCar(string licensePlate, DateTime date) : base(licensePlate, date)
+        {
+        }
+
+        /// <summary>
+        /// Overrides the Price methode to apply additional discount on weekends (15%).
+        /// BroBizz dicount applies after the weekend discount.
+        /// </summary>
+        /// <returns>Final price after weeknd dicount</returns>
+        public override double Price()
+        {
+            double price = base.Price();
+            //Apply a 15% discount if the vehicle crosses on weekends
+            if (Date.DayOfWeek == DayOfWeek.Saturday || Date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                price *= 0.85; //Weekend dicount 
+            }
+
+            //Apply the BroBizz discount after the weekend discount
+
+            if (BroBizz)
+            {
+                price *= 0.9; //BroBizz discount 10%
+            }
+
+            return price;
+        }
+    }
+}
