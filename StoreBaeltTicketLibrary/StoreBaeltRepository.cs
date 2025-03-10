55cf44668df803ca3cba332aa13f046262b7f11c
Rebekka Rendow Junget <rej003@edu.zealand.dk>
2025-03-10 12:29:31 +0100

    Created StroeBaeltRepository With Add, GetAll and GetTicketByLicensePlate methods

---
+++ StoreBaeltTicketLibrary/StoreBaeltRepository.cs
@@ -0,0 +1,45 @@
+﻿using BridgeClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreBaeltTicketLibrary
+{
+    public class StoreBaeltRepository
+    {
+        private List<Vehicle> _tickets = new List<Vehicle>();
+
+        /// <summary>
+        /// Adds a new ticket(car) to the repository
+        /// </summary>
+        /// <param name="car">The ticket to be added</param>
+
+        public void AddTicket(Vehicle vehicle)
+        {
+            _tickets.Add(vehicle);
+        }
+
+        /// <summary>
+        /// Gets all tickets from the repository
+        /// </summary>
+        /// <returns>A list of all StoreBaeltCar tickets</returns>
+
+        public List<Vehicle> GetAllTickets()
+        {
+            return _tickets;
+        }
+         
+
+        /// <summary>
+        /// Gets all tickets from the repository by license plate
+        /// </summary>
+        /// <param name="licensePlate"></param>
+        /// <returns>List of StoreBaeltCar ticket for the specified lincens plate</returns>
+        public List<Vehicle> GetTicketByLicensePlate(string licensePlate)
+        {
+            return _tickets.Where(vehicle => vehicle.Licenseplate == licensePlate).ToList();
+        }
+    }
+}
